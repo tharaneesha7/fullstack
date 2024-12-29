ca159c16ee8ebe5d03682327851d79ffc438f656
tharaneesha7 <tharaneesha96@gmail.com>
2024-12-29 22:45:26 +0530

    day3 react+vite

---
+++ fullstackPlacement/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import ClassCompEg from './Components/ClassComponents/classCompEg'
+import DefaultExplanation from './Components/FunctionalComponents/DefaultExplanation'
+import UnorderedList from './Components/FunctionalComponents/UnorderedList'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <ClassCompEg />
+        <DefaultExplanation />
+        <UnorderedList />
+       </div>  )
+}
+
+export default App
